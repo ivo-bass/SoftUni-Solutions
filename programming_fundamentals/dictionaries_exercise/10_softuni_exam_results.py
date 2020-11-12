470efc9be2062a42e92f19dd52680ba714da8296
ivo-bass <ivailo.ignatoff@gmail.com>
2020-11-12 16:10:39 +0200

    add solutions for:    9. ForceBook   10. SoftUni Exam Results

---
+++ programming_fundamentals/dictionaries_exercise/10_softuni_exam_results.py
@@ -0,0 +1,60 @@
+class Exam:
+	def __init__(self):
+		self.max_results = {}
+		self.submissions = {}
+
+	def ban(self, name):
+		if name in self.max_results:
+			del self.max_results[name]
+
+	def add_student(self, name):
+		if name not in self.max_results:
+			self.max_results[name] = 0
+
+	def add_language(self, lang):
+		if lang not in self.submissions:
+			self.submissions[lang] = 0
+
+	def add_result(self, name, lang, points):
+		self.submissions[lang] += 1
+		if points > self.max_results[name]:
+			self.max_results[name] = points
+
+	def sort_results(self):
+		self.max_results = dict(sorted(self.max_results.items(), key=lambda x: x[0]))
+		self.max_results = dict(sorted(self.max_results.items(), key=lambda x: x[1], reverse=True))
+
+	def sort_submissions(self):
+		self.submissions = dict(sorted(self.submissions.items(), key=lambda x: x[0]))
+		self.submissions = dict(sorted(self.submissions.items(), key=lambda x: x[1], reverse=True))
+
+	def get_statistics(self):
+		self.sort_results()
+		self.sort_submissions()
+		print("Results:")
+		for name, points in self.max_results.items():
+			print(f"{name} | {points}")
+		print("Submissions:")
+		for lang, count in self.submissions.items():
+			print(f"{lang} - {count}")
+
+
+def main():
+	exam = Exam()
+	data = input()
+	while not data == "exam finished":
+		command = data.split("-")
+		name = command[0]
+		if command[1] == "banned":
+			exam.ban(name)
+		else:
+			language, points = command[1], int(command[2])
+			exam.add_student(name)
+			exam.add_language(language)
+			exam.add_result(name, language, points)
+		data = input()
+	exam.get_statistics()
+
+
+if __name__ == '__main__':
+	main()
---
+++ programming_fundamentals/dictionaries_exercise/9_forcebook.py
@@ -0,0 +1,59 @@
+class ForceBook:
+	def __init__(self):
+		self.sides = {}
+		self.all_users = []
+
+	def add_user(self, side, user):
+		if side not in self.sides:
+			self.sides[side] = [user]
+			self.all_users.append(user)
+		else:
+			if user not in self.all_users:
+				self.all_users.append(user)
+				self.sides[side].append(user)
+
+	def change_side(self, side, user):
+		for key, value in self.sides.items():
+			if user in value and not side == key:
+				self.sides[key].remove(user)
+				if side not in self.sides:
+					self.sides[side] = [user]
+				else:
+					self.sides[side].append(user)
+				break
+
+	def get_info(self):
+		self.sides = dict(sorted(self.sides.items(), key=lambda x: x[0]))
+		self.sides = dict(sorted(self.sides.items(), key=lambda x: len(x[1]), reverse=True))
+		for side, users in self.sides.items():
+			count = len(users)
+			if count > 0:
+				print(f"Side: {side}, Members: {count}")
+				users = list(sorted(users))
+				for user in users:
+					print(f"! {user}")
+
+
+def main():
+	f_b = ForceBook()
+	data = input()
+	while not data == "Lumpawaroo":
+		if " | " in data:
+			command = data.split(" | ")
+			side, user = command[0], command[1]
+			if user not in f_b.all_users:
+				f_b.add_user(side, user)
+		elif " -> " in data:
+			command = data.split(" -> ")
+			user, side = command[0], command[1]
+			if user in f_b.all_users:
+				f_b.change_side(side, user)
+			elif user not in f_b.all_users:
+				f_b.add_user(side, user)
+			print(f"{user} joins the {side} side!")
+		data = input()
+	f_b.get_info()
+
+
+if __name__ == '__main__':
+	main()
