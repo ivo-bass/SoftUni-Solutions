85561ff07aed37838b3d28cab9cca38e33cb9442
Ivailo Ignatov <57629471+ivo-bass@users.noreply.github.com>
2020-12-13 19:45:17 +0200

    Update

--- programming_fundamentals/_EXAM_/2_.py
+++ programming_fundamentals/_EXAM_/2_.py
@@ -3,11 +3,11 @@ from re import match
 successful = 0
 n = int(input())
 for _ in range(n):
-	matches = match(r"^(U\$([A-Z][a-z]{2,})U\$P@\$([A-Za-z]{5,}[\d]+)P@\$)$", input())
-	if matches:
+	valid = match(r"^(U\$([A-Z][a-z]{2,})U\$P@\$([A-Za-z]{5,}[\d]+)P@\$)$", input())
+	if valid:
 		successful += 1
 		print("Registration was successful")
-		print(f"Username: {matches[2]}, Password: {matches[3]}")
+		print(f"Username: {valid[2]}, Password: {valid[3]}")
 	else:
 		print("Invalid username or password")
 
