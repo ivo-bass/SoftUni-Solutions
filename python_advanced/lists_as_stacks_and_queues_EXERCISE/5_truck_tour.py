8f5b65be44bceedae59d2383f4cb4410862336c6
ivo-bass <ivailo.ignatoff@gmail.com>
2021-01-12 23:46:55 +0200

    solve with 1 que

--- python_advanced/lists_as_stacks_and_queues_EXERCISE/5_truck_tour.py
+++ python_advanced/lists_as_stacks_and_queues_EXERCISE/5_truck_tour.py
@@ -1,32 +1,22 @@
 from collections import deque
 
-q_fuel = deque()
-q_dist = deque()
+q = deque()
 
 pumps_count = int(input())
-
 for _ in range(pumps_count):
-    # Fill the queues
-    info = input().split(' ')
-    amount, distance = int(info[0]), int(info[1])
-    q_fuel.append(amount)
-    q_dist.append(distance)
+    q.append(input())
 
 index = 0
 success = False
 while True:
     fuel_in_tank = 0
-    # copies of the dequeues are needed to operate
-    q_f_copy = q_fuel.copy()
-    q_d_copy = q_dist.copy()
+    # copy of the que is needed to operate with
+    q_copy = q.copy()
     for _ in range(pumps_count):  # The amount of steps in the circle == pumps_count
-        # First we take the properties of the current pump
-        current_amount = q_f_copy.popleft()
-        next_distance = q_d_copy.popleft()
-        # Then we add the ammount to the tank
-        fuel_in_tank += current_amount
-        # Then we extract the distance from the tank
-        fuel_in_tank -= next_distance
+        amount, distance = q_copy.popleft().split(' ')
+        amount, distance = int(amount), int(distance)
+        fuel_in_tank += amount
+        fuel_in_tank -= distance
         # If the fuel is negative, the tank cannot proceed
         if fuel_in_tank < 0:
             success = False
@@ -36,11 +26,8 @@ while True:
     if success:
         break
     # If the circle is not closed we try starting from next index
-    q_fuel.append(q_fuel.popleft())
-    q_dist.append(q_dist.popleft())
+    q.append(q.popleft())
     index += 1
-
 print(index)
 
-# The complexity is On
-# TODO: optimize
+# The complexity is O(n^2)
