78c56e048727084e7ed8f3555adbafe3a362fc82
ivo-bass <ivailo.ignatoff@gmail.com>
2021-01-28 15:59:17 +0200

    exam prep 3 swap if statements

--- python_advanced/EXAM_PREP/3.py
+++ python_advanced/EXAM_PREP/3.py
@@ -10,13 +10,11 @@ def generate_row(ll):
     next_row = []
     prev_row = ll[-1]
     for i in range(len(prev_row)+1):
-        if i - 1 < 0:
-            left_num = 0
-        else:
+        left_num = 0
+        right_num = 0
+        if i - 1 >= 0:
             left_num = prev_row[i - 1]
-        if i == len(prev_row):
-            right_num = 0
-        else:
+        if i < len(prev_row):
             right_num = prev_row[i]
         next_row.append(left_num + right_num)
     return next_row
