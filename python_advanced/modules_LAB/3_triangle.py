d5a16d47140b94be429d1363aeb84eec5ce726a8
ivo-bass <ivailo.ignatoff@gmail.com>
2021-02-01 23:28:58 +0200

    trying to make packages

---
+++ python_advanced/modules_LAB/3_triangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from python_advanced.modules_LAB.triangle.draw import print_triangle
+
+size = int(input())
+print_triangle(size)
---
+++ python_advanced/modules_LAB/triangle/draw.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+def print_line(n):
+    ll = []
+    for i in range(1, n+1):
+        ll.append(i)
+    print(' '.join(map(str, ll)))
+
+
+def print_triangle(size):
+    for row in range(1, size + 2):
+        print_line(row)
+    for row in range(size, 0, -1):
+        print_line(row)
+
+
+print_triangle(5)
