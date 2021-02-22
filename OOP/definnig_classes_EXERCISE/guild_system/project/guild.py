c23828b6b04f0266fabd317f5e3ff2feb8a009d9
ivo-bass <ivailo.ignatoff@gmail.com>
2021-02-22 16:29:47 +0200

    vesela kymitva s basha

---
+++ OOP/definnig_classes_EXERCISE/guild_system/project/guild.py
@@ -0,0 +1,45 @@
+class Guild:
+    def __init__(self, name: str):
+        self.name = name
+        self.players = []
+
+    def assign_player(self, p):
+        if p.guild == 'Unaffiliated':
+            p.guild = self.name
+            self.players.append(p)
+            return f"Welcome player {p.name} to the guild {self.name}"
+        if p.guild == self.name:
+            return f"Player {p.name} is already in the guild."
+        return f"Player {p.name} is in another guild."
+
+    def kick_player(self, player_name):
+        for p in self.players:
+            if p.name == player_name:
+                p.guild = 'Unaffiliated'
+                self.players.remove(p)
+                return f"Player {player_name} has been removed from the guild."
+        return f"Player {player_name} is not in the guild."
+
+    def guild_info(self):
+        g_info = f"Guild: {self.name}\n"
+        pl_info = ''
+        for pl in self.players:
+            pl_info += f'{pl.player_info()}\n'
+        return g_info + pl_info
+
+# george = Player("George", 50, 100)
+# ivo = Player("Ivo", 150, 200)
+# print(george.add_skill("Shield Break", 20))
+# print(ivo.add_skill('sth', 100))
+# print(george.player_info())
+# print(ivo.player_info())
+# guild = Guild("UGT")
+# guild2 = Guild("G2")
+# print(guild.assign_player(george))
+# print(guild2.assign_player(ivo))
+# print(guild.kick_player(george.name))
+# print(guild2.assign_player(george))
+# print(guild2.kick_player(ivo.name))
+# print(ivo.player_info())
+# print(guild.guild_info())
+# print(guild2.guild_info())
---
+++ OOP/definnig_classes_EXERCISE/guild_system/project/player.py
@@ -0,0 +1,29 @@
+class Player:
+    def __init__(self, name: str, hp: int, mp: int):
+        self.name = name
+        self.hp = hp
+        self.mp = mp
+        self.skills = {}
+        self.guild = 'Unaffiliated'
+
+    def add_skill(self, skill_name: str, mana_cost: int):
+        if skill_name in self.skills.keys():
+            return "Skill already added"
+        self.skills[skill_name] = mana_cost
+        return f"Skill {skill_name} added to the collection of the player {self.name}"
+
+    def player_info(self):
+        pl_info = f'Name: {self.name}\n' \
+                  f'Guild: {self.guild}\n' \
+                  f'HP: {self.hp}\n' \
+                  f'MP: {self.mp}\n'
+
+        sk_info = ''
+        for sk_name, sk_cost in self.skills.items():
+            sk_info += f'==={sk_name} - {sk_cost}\n'
+        return pl_info + sk_info
+
+# player = Player('ivo', 50, 50)
+# player.add_skill('mana', 10)
+# player.add_skill('new', 100)
+# print(player.player_info())
