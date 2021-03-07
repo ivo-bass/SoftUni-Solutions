9a609911f149af8a6b943dcc8bf2e16fc19e1112
ivo-bass <ivailo.ignatoff@gmail.com>
2021-03-08 00:04:00 +0200

    inheritance ex person solution

---
+++ OOP/inheritance_EXERCISE/person/project/child.py
@@ -0,0 +1,24 @@
+from project.person import Person
+
+
+class Child(Person):
+    def __init__(self, name: str, age: int):
+        super().__init__(name, age)
+
+# # zero test
+# from project.person import Person
+# from project.child import Child
+# import unittest
+#
+#
+# class Tests(unittest.TestCase):
+#     def test(self):
+#         person = Person("Peter", 25)
+#         child = Child("Peter Junior", 5)
+#         self.assertEqual(person.name, "Peter")
+#         self.assertEqual(person.age, 25)
+#         self.assertEqual(child.__class__.__bases__[0].__name__, "Person")
+#
+#
+# if __name__ == "__main__":
+#     unittest.main()
