9119e916058667387824331345e375546a35a7e6
ivo-bass <ivailo.ignatoff@gmail.com>
2021-03-09 23:13:51 +0200

    refactor need for speed

--- OOP/inheritance_EXERCISE/need_for_speed/project/car.py
+++ OOP/inheritance_EXERCISE/need_for_speed/project/car.py
@@ -3,5 +3,3 @@ from project.vehicle import Vehicle
 
 class Car(Vehicle):
     DEFAULT_FUEL_CONSUMPTION = 3
-
-    pass
--- OOP/inheritance_EXERCISE/need_for_speed/project/race_motorcycle.py
+++ OOP/inheritance_EXERCISE/need_for_speed/project/race_motorcycle.py
@@ -3,5 +3,3 @@ from project.motorcycle import Motorcycle
 
 class RaceMotorcycle(Motorcycle):
     DEFAULT_FUEL_CONSUMPTION = 8
-
-    pass
