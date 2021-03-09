a2bb9b16977bb3e880548c34846b842007161092
ivo-bass <ivailo.ignatoff@gmail.com>
2021-03-10 00:53:52 +0200

    refactor image_area.py

--- OOP/polymorphysm_LAB/image_area.py
+++ OOP/polymorphysm_LAB/image_area.py
@@ -2,24 +2,25 @@ class ImageArea:
     def __init__(self, width, height):
         self.width = width
         self.height = height
+        self.area = self.get_area()
 
     def __gt__(self, other):
-        return self.get_area() > other.get_area()
+        return self.area > other.area
 
     def __ge__(self, other):
-        return self.get_area() >= other.get_area()
+        return self.area >= other.area
 
     def __lt__(self, other):
-        return self.get_area() < other.get_area()
+        return self.area < other.area
 
     def __le__(self, other):
-        return self.get_area() <= other.get_area()
+        return self.area <= other.area
 
     def __eq__(self, other):
-        return self.get_area() == other.get_area()
+        return self.area == other.area
 
     def __ne__(self, other):
-        return self.get_area() != other.get_area()
+        return self.area != other.area
 
     def get_area(self):
         return self.width * self.height
