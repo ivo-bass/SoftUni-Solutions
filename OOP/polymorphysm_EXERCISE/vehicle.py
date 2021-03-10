d3b1eeeff211fead6c7e66216de404d3752b3592
ivo-bass <ivailo.ignatoff@gmail.com>
2021-03-11 00:40:57 +0200

    polymorphysm vehicle solution

---
+++ OOP/polymorphysm_EXERCISE/vehicle.py
@@ -0,0 +1,55 @@
+from abc import ABC, abstractmethod
+
+
+class Vehicle(ABC):
+    AC_CONSUMPTION = 0
+
+    @abstractmethod
+    def drive(self, distance):
+        pass
+
+    @abstractmethod
+    def refuel(self, fuel):
+        pass
+
+
+class Car(Vehicle):
+    AC_CONSUMPTION = 0.9
+
+    def __init__(self, fuel_quantity, fuel_consumption):
+        self.fuel_quantity = fuel_quantity
+        self.fuel_consumption = fuel_consumption
+
+    def drive(self, distance):
+        if self.fuel_quantity >= (self.fuel_consumption + self.AC_CONSUMPTION) * distance:
+            self.fuel_quantity -= (self.fuel_consumption + self.AC_CONSUMPTION) * distance
+
+    def refuel(self, fuel):
+        self.fuel_quantity += fuel
+
+
+class Truck(Vehicle):
+    AC_CONSUMPTION = 1.6
+
+    def __init__(self, fuel_quantity, fuel_consumption):
+        self.fuel_quantity = fuel_quantity
+        self.fuel_consumption = fuel_consumption
+
+    def drive(self, distance):
+        if self.fuel_quantity >= (self.fuel_consumption + self.AC_CONSUMPTION) * distance:
+            self.fuel_quantity -= (self.fuel_consumption + self.AC_CONSUMPTION) * distance
+
+    def refuel(self, fuel):
+        self.fuel_quantity += 0.95 * fuel
+
+# car = Car(20, 5)
+# car.drive(3)
+# print(car.fuel_quantity)
+# car.refuel(10)
+# print(car.fuel_quantity)
+#
+# truck = Truck(100, 15)
+# truck.drive(5)
+# print(truck.fuel_quantity)
+# truck.refuel(50)
+# print(truck.fuel_quantity)
