18a226d971e177a2852dcb06a90c9c13a10b37e2
ivo-bass <ivailo.ignatoff@gmail.com>
2021-03-12 02:11:32 +0200

    refactor vehicle no constants

--- OOP/polymorphysm_EXERCISE/vehicle.py
+++ OOP/polymorphysm_EXERCISE/vehicle.py
@@ -2,30 +2,28 @@ from abc import ABC, abstractmethod
 
 
 class Vehicle(ABC):
-    AC_CONSUMPTION = 0
-
     def __init__(self, fuel_quantity, fuel_consumption):
         self.fuel_quantity = fuel_quantity
         self.fuel_consumption = fuel_consumption
 
-    def drive(self, distance):
-        if self.fuel_quantity >= (self.fuel_consumption + self.AC_CONSUMPTION) * distance:
-            self.fuel_quantity -= (self.fuel_consumption + self.AC_CONSUMPTION) * distance
-
     @abstractmethod
-    def refuel(self, fuel):
+    def drive(self, distance):
         pass
-
-
-class Car(Vehicle):
-    AC_CONSUMPTION = 0.9
 
     def refuel(self, fuel):
         self.fuel_quantity += fuel
 
 
+class Car(Vehicle):
+    def drive(self, distance):
+        if self.fuel_quantity >= (self.fuel_consumption + 0.9) * distance:
+            self.fuel_quantity -= (self.fuel_consumption + 0.9) * distance
+
+
 class Truck(Vehicle):
-    AC_CONSUMPTION = 1.6
+    def drive(self, distance):
+        if self.fuel_quantity >= (self.fuel_consumption + 1.6) * distance:
+            self.fuel_quantity -= (self.fuel_consumption + 1.6) * distance
 
     def refuel(self, fuel):
         self.fuel_quantity += 0.95 * fuel
