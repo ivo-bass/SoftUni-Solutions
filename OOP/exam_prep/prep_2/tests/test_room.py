7bc9a156ff881fbd3571e8924284e64e61d3c124
Ivailo Ignatov <ivobass85@gmail.com>
2021-03-24 18:32:36 +0200

    refactor tests for exam prep2 with setUp method

--- OOP/exam_prep/prep_2/tests/test_room.py
+++ OOP/exam_prep/prep_2/tests/test_room.py
@@ -6,29 +6,29 @@ from project.rooms.room import Room
 
 
 class TestRoom(unittest.TestCase):
+    def setUp(self) -> None:
+        self.r = Room('a', 12.2, 2)
+        self.child_one = Child(5, 1, 2, 1)
+        self.child_two = Child(3, 2)
+
     def test_init(self):
-        r = Room('a', 12.2, 2)
-        self.assertEqual(r.family_name, 'a')
-        self.assertEqual(r.budget, 12.2)
-        self.assertEqual(r.members_count, 2)
-        self.assertEqual(r.children, [])
-        self.assertEqual(r.expenses, 0)
+        self.assertEqual(self.r.family_name, 'a')
+        self.assertEqual(self.r.budget, 12.2)
+        self.assertEqual(self.r.members_count, 2)
+        self.assertEqual(self.r.children, [])
+        self.assertEqual(self.r.expenses, 0)
 
     def test_invalid_expenses(self):
-        r = Room('a', 12.2, 2)
         try:
-            r.expenses = -1
-        except ValueError:
+            self.r.expenses = -1
+        except Exception:
             self.assertRaises(ValueError)
 
     def test_calculate_expenses(self):
-        child_one = Child(5, 1, 2, 1)
-        child_two = Child(3, 2)
-        r = Room("Peterson", 600, 520)
-        r.children.append(child_one)
-        r.children.append(child_two)
-        r.calculate_expenses(r.children)
-        self.assertEqual(r.expenses, 420)
+        self.r.children.append(self.child_one)
+        self.r.children.append(self.child_two)
+        self.r.calculate_expenses(self.r.children)
+        self.assertEqual(self.r.expenses, 420)
 
 
 if __name__ == '__main__':
